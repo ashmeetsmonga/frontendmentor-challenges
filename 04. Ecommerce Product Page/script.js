45a037d7e68399069176e742bf6114d3c7349e11
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-14 20:04:03 +0530

    modal added

--- 04. Ecommerce Product Page/script.js
+++ 04. Ecommerce Product Page/script.js
@@ -5,12 +5,12 @@ function changeImage() {
 	thumbnailImages.forEach((image) => {
 		const parentDiv = image.parentElement;
 		image.classList.remove("opacity-50");
-		parentDiv.classList.remove("outline", "outline-2", "outline-orange", "rounded-lg");
+		parentDiv.classList.remove("outline", "outline-2", "outline-orange");
 	});
 
 	const parentDiv = this.parentElement;
 	this.classList.add("opacity-50");
-	parentDiv.classList.add("outline", "outline-2", "outline-orange", "rounded-lg");
+	parentDiv.classList.add("outline", "outline-2", "outline-orange");
 
 	const mainImageSrc = this.src.replace("-thumbnail", "");
 	mainImage.src = mainImageSrc;
