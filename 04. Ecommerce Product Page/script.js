5d60277601648e7810735cc438aae35f7bde4803
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-14 19:37:11 +0530

    change image on thumbnail click added

---
+++ 04. Ecommerce Product Page/script.js
@@ -0,0 +1,19 @@
+const mainImage = document.getElementById("main-image");
+const thumbnailImages = document.querySelectorAll(".thumbnail-image");
+
+function changeImage() {
+	thumbnailImages.forEach((image) => {
+		const parentDiv = image.parentElement;
+		image.classList.remove("opacity-50");
+		parentDiv.classList.remove("outline", "outline-2", "outline-orange", "rounded-lg");
+	});
+
+	const parentDiv = this.parentElement;
+	this.classList.add("opacity-50");
+	parentDiv.classList.add("outline", "outline-2", "outline-orange", "rounded-lg");
+
+	const mainImageSrc = this.src.replace("-thumbnail", "");
+	mainImage.src = mainImageSrc;
+}
+
+thumbnailImages.forEach((image) => image.addEventListener("click", changeImage));
